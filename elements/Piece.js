c29ad555d6d1d870efdb0dea10176cfaec27090c
ADSmith-0 <adam00199382818@gmail.com>
2021-10-21 16:59:21 +0100

    update: basic pawn movement implemented

--- elements/Piece.js
+++ elements/Piece.js
@@ -1,17 +1,27 @@
 class Piece extends HTMLElement {
     constructor(){
         super();
+
+        this._firstMove = true;
+        this._currentTile = this.parentElement.id;
+        this._x = this._currentTile[0];
+        this._y = this._currentTile[1];
         
         this._handleDrag = this._handleDrag.bind(this);
         this._trackCoords = this._trackCoords.bind(this);
         this._stopDrag = this._stopDrag.bind(this);
+        this._moveIsValid = this._moveIsValid.bind(this);
+        this._updateLocation = this._updateLocation.bind(this);
+        this._calculateValidSpaces = this._calculateValidSpaces.bind(this);
+
+        this._validSpaces = this._calculateValidSpaces();
+
         this.addEventListener('mousedown', this._handleDrag);
     }
     _handleDrag(e){
         e.preventDefault();
         document.addEventListener('mousemove', this._trackCoords);
         document.addEventListener('mouseup', this._stopDrag);
-        this._currentTile = e.target.parentElement;
         this._grabbedPiece = e.target;
         this._toggleGrabbing();
     }
@@ -31,8 +41,8 @@ class Piece extends HTMLElement {
         document.removeEventListener('mouseup', this._stopDrag);
         this._grabbedPiece.style.transform = "translate(0,0)";
         this._toggleGrabbing();
-        const hoveringOverTile = this._cleanTile(document.elementFromPoint(e.clientX, e.clientY));
-        this._movePiece(hoveringOverTile);
+        this._hoveringOverTile = this._cleanTile(document.elementFromPoint(e.clientX, e.clientY));
+        this._movePiece(this._hoveringOverTile);
     }
     _toggleGrabbing(){
         this._grabbedPiece.classList.toggle('grabbing');
@@ -41,13 +51,30 @@ class Piece extends HTMLElement {
         if(tile.id != this._currentTile && this._moveIsValid()){
             if(tile.firstElementChild) tile.firstElementChild.remove();
             tile.appendChild(this._grabbedPiece);
+            if(this._firstMove) this._firstMove = false;
+            this._updateLocation();
         }
+    }
+    _updateLocation(){
+        this._currentTile = this._hoveringOverTile.id;
+        this._x = this._currentTile[0];
+        this._y = this._currentTile[1];
+        this._validSpaces = this._calculateValidSpaces();
     }
     _cleanTile(tile){
         return (tile.localName == "chess-piece") ? tile.parentElement : tile;
     }
     _moveIsValid(){
-        return true;
+        return this._calculateValidSpaces();
+    }
+    _calculateValidSpaces(){
+        return [];
+    }
+    _numberFromLetter(letter){
+        return (letter.charCodeAt(0)-64);
+    }
+    _letterFromNumber(number){
+        return String.fromCharCode(number+64);
     }
 }
 customElements.define('chess-piece', Piece);
--- elements/pieces/pawn.js
+++ elements/pieces/pawn.js
@@ -3,7 +3,18 @@ class Pawn extends Piece {
         super();
     }
     _moveIsValid(){
-        return false;
+        // console.log(this._validSpaces);
+        const arr = this._validSpaces.filter(space => space === this._hoveringOverTile.id);
+        return arr.length;
+    }
+    _calculateValidSpaces(){
+        const moves = [];
+        const limit = +this._firstMove+1;
+        for(let i = 1; i <= limit; i++){
+            const newY = (this.classList.contains('w')) ? parseInt(this._y)+i : parseInt(this._y)-i;
+            moves.push(this._x+newY);
+        }
+        return moves;
     }
 }
 customElements.define('pawn-x', Pawn);
