2f7685884c762ac133d78618df00a98268f71898
ADSmith-0 <adam00199382818@gmail.com>
2021-11-03 17:07:14 +0000

    update: finished rook movement

--- elements/pieces/rook.js
+++ elements/pieces/rook.js
@@ -2,5 +2,18 @@ class Rook extends Piece {
     constructor(){
         super();
     }
+    _calculatePossibleMoves(){
+        const moves = [
+            [0,1],
+            [1,0],
+            [0,-1],
+            [-1,0]
+        ];
+
+        return moves.map(move => {
+            const [x, y] = move;
+            return [1,2,3,4,5,6,7,8].map(coeff => [x*coeff, y*coeff])
+        }).reduce((acc, val) => [...acc, ...val], []);
+    }
 }
 customElements.define('rook-x', Rook);
