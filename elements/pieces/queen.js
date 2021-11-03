b9e2fe2055dc57cec88245c4d334397eb2233874
ADSmith-0 <adam00199382818@gmail.com>
2021-11-03 17:19:22 +0000

    update: queen movement added

--- elements/pieces/queen.js
+++ elements/pieces/queen.js
@@ -2,8 +2,14 @@ class Queen extends Piece {
     constructor(){
         super();
     }
-    _moveIsValid(){
-        return true;
+    _calculatePossibleMoves(){
+        const directions = [
+            [-1,1],  [0,1],  [1,1],
+            [-1,0],          [1,0],
+            [-1,-1], [0,-1], [1,-1]
+        ];
+
+        return this._allMovesFromDirections(directions);
     }
 }
 customElements.define('queen-x', Queen);
