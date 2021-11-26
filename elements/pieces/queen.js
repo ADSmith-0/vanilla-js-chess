c9ba1b94aa065e32d8b80069ab7ed350a4f04098
ADSmith-0 <adam00199382818@gmail.com>
2021-11-26 17:05:48 +0000

    update: fixing piece movement

--- elements/pieces/queen.js
+++ elements/pieces/queen.js
@@ -1,11 +1,11 @@
 class Queen extends Piece {
     constructor(){
         super();
-        // this._directions = [
-        //     [-1,1],  [0,1],  [1,1],
-        //     [-1,0],          [1,0],
-        //     [-1,-1], [0,-1], [1,-1]
-        // ];
+        this._directions = [
+            [-1,1],  [0,1],  [1,1],
+            [-1,0],          [1,0],
+            [-1,-1], [0,-1], [1,-1]
+        ];
     }
 }
 customElements.define('queen-x', Queen);
--- elements/pieces/rook.js
+++ elements/pieces/rook.js
@@ -2,12 +2,12 @@ class Rook extends Piece {
     constructor(){
         super();
 
-        // this._directions = [
-        //     [0,1],
-        //     [1,0],
-        //     [0,-1],
-        //     [-1,0]
-        // ];
+        this._directions = [
+            [0,1],
+            [1,0],
+            [0,-1],
+            [-1,0]
+        ];
     }
 }
 customElements.define('rook-x', Rook);
