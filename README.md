504067bc4162dd69a913c98d9e11d643f9a21eac
ADSmith-0 <adam00199382818@gmail.com>
2021-12-28 15:42:52 +0000

    update: refactoring all code

--- README.md
+++
@@ -1,8 +0,0 @@
-# Vanilla Chess
-## Chess built with:
-- Vanilla JavaScript
-- Web Components API (custom elements)
-- OOP
-
-## How To Run:
-Download the repo and run it through a server, I use the VSCode plugin LiveServer in the root folder
--- elements/Chessboard.js
+++ elements/Chessboard.js
@@ -1,81 +1,25 @@
 class Chessboard extends HTMLElement {
     constructor(){
         super();
-        this._pieceSize = "s";
-        this._backRow = {
-            "A": "rook",
-            "B": "knight",
-            "C": "bishop",
-            "D": "queen",
-            "E": "king",
-            "F": "bishop",
-            "G": "knight",
-            "H": "rook",
-        }
-        this._pressureMap = new Map();
-        this._pressureMap.set({"H": ["","b","b","b","b","b","b",""]});
-        this._pressureMap.set({"G": ["b","b","b","b","b","b","b","b","b"]});
-        this._pressureMap.set({"F": ["","","","","","","",""]});
-        this._pressureMap.set({"E": ["","","","","","","",""]});
-        this._pressureMap.set({"D": ["","","","","","","",""]});
-        this._pressureMap.set({"C": ["","","","","","","",""]});
-        this._pressureMap.set({"B": ["w","w","w","w","w","w","w","w","w"]});
-        this._pressureMap.set({"A": ["","w","w","w","w","w","w",""]});
-        this._blackChecked = false;
-        this._whiteChecked = false;
         this._init();
     }
     _init(){
-        for(let i = 8; i >= 1; i--){
-            const style = (i%2) ?  "row" : "alt-row";
-            this.appendChild(this._createRow(i, style));
+        for(let rank = 8; rank >= 1; rank--){
+            for(let file = 1; file <= 8; file++){
+                const tile = this._createTile(file, rank);
+                this.appendChild(tile);
+            }
         }
     }
-    _createRow(yCoord, style){
-        const row = document.createElement('section');
-        row.setAttribute('id', yCoord);
-        row.setAttribute('class', style);
-        const x = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
-        x.map(xCoord => row.appendChild(this._createTile(xCoord, yCoord)));
-        return row;
-    }
-    _createTile(xCoord, yCoord){
+    _createTile(file, rank){
         const tile = document.createElement('div');
-        tile.setAttribute('id', xCoord+yCoord.toString());
-        tile.setAttribute('class', 'tile');
-        if(yCoord <= 2 || yCoord >= 7){
-            const piece = this._getPieceFromCoordinates(xCoord, yCoord);
-            tile.appendChild(this._createPiece(piece));
-        }
+        tile.setAttribute('id', file.toString()+rank.toString());
+        
+        let className = "tile ";
+        className += ((file+rank) % 2) ? "light-square" : "dark-square";
+        tile.setAttribute('class', className);
+
         return tile;
     }
-    _getPieceFromCoordinates(xCoord, yCoord){
-        if(yCoord == 7){
-            return ["b", "pawn"];
-        }else if(yCoord == 2){
-            return ["w", "pawn"];
-        }else if(yCoord == 1 || yCoord == 8){
-            let piece = [];
-            piece.push((yCoord == 1) ? "w" : "b");
-            piece.push(this._backRow[xCoord]);
-            return piece;
-        }
-        return null;
-    }
-    _createPiece(pieceCode){
-        let piece;
-        piece = document.createElement(`${pieceCode[1]}-x`);
-        piece.setAttribute('class', `chess-piece ${pieceCode[0]} ${this._pieceSize}`);
-        return piece;
-    }
-    static checked(colour){
-        if(colour === "b"){
-            this._blackChecked = true;
-        }else{
-            this._whiteChecked = true;
-        }
-        console.table([["White Checked:", this._whiteChecked], ["Black Checked:", this._blackChecked]]);
-    }
 }
-
-customElements.define('chessboard-x', Chessboard);
+window.customElements.define('chessboard-x', Chessboard);
