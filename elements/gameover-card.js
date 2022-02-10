5f0ce63bf00c3ae07f8e409d0676f20b112170fe
ADSmith-0 <adam00199382818@gmail.com>
2022-02-10 13:58:37 +0000

    update: gameover card can now display multiple different messages based on game outcome

--- elements/gameover-card.js
+++ elements/gameover-card.js
@@ -2,7 +2,10 @@ class GameoverCard extends HTMLElement{
     constructor(){
         super();
 
-        const message = "White wins!";
+        const ending = this.getAttribute("ending") || "ds";
+
+        const message = this.#messageFromEnding(ending);
+
         this.innerHTML = `
             <section id="gameover-card">
                 <button id="goc-close">✖</button>
@@ -10,6 +13,33 @@ class GameoverCard extends HTMLElement{
                 <button id="goc-rematch" class="button">Rematch</button>
             </section>
         `;
+
+        this.close();
+    }
+
+    #messageFromEnding(ending){
+        switch(ending){
+            case "wc":
+                return "White wins <br> by checkmate!";
+            case "bc":
+                return "Black wins <br> by checkmate!";
+            case "wr":
+                return "White wins <br> by resignation!";
+            case "br":
+                return "Black wins <br> by resignation!";
+            case "ds":
+                return "Draw <br> by stalemate";
+            case "dr":
+                return "Draw <br> by repetition";
+        }
+    }
+
+    open(){
+        this.style.display = "block";
+    }
+
+    close(){
+        this.style.display = "none";
     }
 }
 window.customElements.define('gameover-card', GameoverCard);
